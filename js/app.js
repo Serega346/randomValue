84124e47452e273866f9f36ad99f867f0fdd093c
Serega346 <serghomyn@gmail.com>
2020-04-01 23:57:56 +0300

    Reformat

--- js/app.js
+++ js/app.js
@@ -1,14 +1,18 @@
 JsBarcode(".barcode").init();
 const img = document.querySelector('.barcode');
 
-const getRandomInt = (min = 1000, max= 9999) => {
+const getRandomInt = (min = 1000, max = 9999) => {
+
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min)) + min;
+
 };
 
 
 const setValue = () => {
-    img.setAttribute('jsbarcode-value', '482011528'+ getRandomInt());
+
+    img.setAttribute('jsbarcode-value', '482011528' + getRandomInt());
     JsBarcode(".barcode").init();
+
 };
